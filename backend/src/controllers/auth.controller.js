c08633cae97b8eb2200788b7c60e0ccd4e56a731
nithinjayanjasla@gmail.com <nithinjayanjasla@gmail.com>
2025-01-26 12:27:03 +0530

    jwt web token added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,13 +1,44 @@
+import User from "../models/user.models.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { fulName, email, password } = req.body;
+  try {
+    //hash a password
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
+    }
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      fulName,
+      email,
+      password: hashedPassword,
+    });
+
+    if(newUser){
+        // generate jwt  token
+
+    }else{
+        return res.status(400).json({ message: "User not created" });
+    }
 
 
-export const signup = (req, res) => {
-    res.send('Signup route');
-  }
+  } catch (error) {}
+};
 
-  export const login = (req, res) => {
-    res.send('Login route');
-  }
+export const login = (req, res) => {
+  res.send("Login route");
+};
 
-  export const logout = (req, res) => {
-    res.send('Logout route');
-  }
+export const logout = (req, res) => {
+  res.send("Logout route");
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRouters from "./routes/auth.routes.js";
+import { connectDB } from "./lib/database.js";
 
-import express from 'express';
-import  authRouters from './routes/auth.routes.js'
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT;
+app.use(express.json());
 
-app.use('/api/auth',authRouters)
+app.use("/api/auth", authRouters);
 
-app.listen(5001,()=>{
-console.log('server is runing on port')
-})
+app.listen(PORT, () => {
+  console.log("server is runing on PORT:" + PORT);
+  connectDB();
+});
