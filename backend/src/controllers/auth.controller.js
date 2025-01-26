426a707afbaf6831c5e73d629847ecb24a04adfb
nithinjayanjasla@gmail.com <nithinjayanjasla@gmail.com>
2025-01-26 13:10:26 +0530

    first user data added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,14 +1,19 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/user.models.js";
 import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
-  const { fulName, email, password } = req.body;
+  const { fullName, email, password } = req.body; // get data from request body
   try {
     //hash a password
+    if(!fullName || !email || !password){
+        return res.status(400).json({ message: "Please fill all fields" });
+    }
+      
     if (password.length < 6) {
       return res
         .status(400)
-        .json({ message: "Password must be at least 6 characters long" });
+        .json({ message: "Password must be at least 6 characters long" }); // check password length
     }
 
     const user = await User.findOne({ email });
@@ -19,20 +24,26 @@ export const signup = async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
     const newUser = new User({
-      fulName,
+      fullName,
       email,
       password: hashedPassword,
     });
 
-    if(newUser){
-        // generate jwt  token
-
-    }else{
-        return res.status(400).json({ message: "User not created" });
+    if (newUser) {
+      // generate jwt  token
+      generateToken(newUser._id, res); // generate token and set cookie
+      await newUser.save();
+      res.status(201).json({ _id: newUser._id,
+        fullName: newUser.fullName,
+         email: newUser.email ,
+         profilePic: newUser.profilePic,});// send response
+    } else {
+      return res.status(400).json({ message: "User not created" });
     }
-
-
-  } catch (error) {}
+  } catch (error) {
+    console.log("Error in signup controller ", error.message);
+    return res.status(500).json({ message: "Server error" });
+  }
 };
 
 export const login = (req, res) => {
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,11 +1,16 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "6d",// 6 days
+  });
 
-export const  generateToken = (userId,res) => {
+  res.cookie("jwt", token, {
+    maxAge: 6 * 24 * 60 * 60 * 1000, // 6 days
+    httpOnly: true, // cookie cannot be accessed by client side script
+    sameSite: "strict", // cookie cannot be accessed by cross site request
+    secure: process.env.NODE_ENV === "development",
+  });
 
-
-    const token = jwt.sign({userId},process.env.JWT_SECRET,{
-        expiresIn:'6d'
-    });
-
-}
+  return token;
+};
